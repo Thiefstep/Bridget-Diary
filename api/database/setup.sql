cbe1f3a4c87788e50a5c894129ee88fbca6c5d56
Francesca Adu <58713149+FrancescaAdu@users.noreply.github.com>
2023-05-04 15:15:09 +0100

    sql tables created

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -1,9 +1,20 @@
+DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS diary;
 
+CREATE TABLE user (
+    user_id INT GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    password CHAR(60) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
 CREATE TABLE diary (
-    id INT PRIMARY KEY,
-    date DATE,
-    time TIMESTAMP,
-    text VARCHAR(200),
-    category VARCHAR(20)
+    diary_id INT GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL,
+    date DATE NOT NULL,
+    time TIMESTAMP NOT NULL,
+    entry_text VARCHAR(200) NOT NULL,
+    category ENUM('Personal', 'Travel', 'Health', 'Work', 'Other') NOT NULL,
+    PRIMARY KEY (diary_id),
+    FOREIGN KEY (user_id) REFERENCES user("user_id")
 );
