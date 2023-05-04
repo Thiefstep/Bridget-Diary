81a82512aa525094cba0c9e5939eb8e50220c1e4
Thiefstep <matzz1@hotmail.co.uk>
2023-05-04 13:16:00 +0100

    Added basic code

---
+++ api/controllers/diary.js
@@ -0,0 +1,29 @@
+const Diary = require('../models/diary')
+
+async function index(req,res){
+    try{
+        const diary = await Diary.getAll();
+        res.status(200).json(diary)
+    }catch(err){
+        res.status(500).json({
+            'Error': err
+        })
+    }
+}
+
+async function show(req,res){
+    try{
+        const idx = parseInt(req.params.idx)
+        const diary = await Diary.getOneById(idx)
+        res.status(200).json(diary)
+    }catch(err){
+        res.status(404).json({
+            'Error': err
+        })
+    }
+}
+
+
+module.exports = {
+    index, show
+}
