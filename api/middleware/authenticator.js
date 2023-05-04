d2c2e907e269671f76e8a1df61f85c550e38ad31
Thiefstep <matzz1@hotmail.co.uk>
2023-05-04 15:27:41 +0100

    Password made

---
+++ api/middleware/authenticator.js
@@ -0,0 +1,20 @@
+const Token = require('../models/token')
+
+async function authenticator(req, res, next) {
+    try{
+        const userToken = req.headers('authorization')
+
+        if(userToken == 'null'){
+            throw new Error('User not authorised')
+        }else{
+            const validToken = await Token.getOneByToken(userToken)
+            if(validToken){
+                next()
+            }
+        }
+    }catch(err){
+        res.status(403).json({
+            error:err
+        })
+    }
+}
